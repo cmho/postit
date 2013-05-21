8f5b481a0727e69cba88b96593c005f417b97585
Carly Ho <me@carlymho.com>
2013-05-21 15:45:39 -0500

    Fix login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
 	def new
-		
+
 	end
 
 	def create
 		@user = User.find_by_username(params[:username])
 		if @user.authenticate(params[:password])
-			session[:user_id] = user.id
-			redirect_to root_url, :notice => "Welcome, #{user.name}."
+			session[:user_id] = @user.id
+			redirect_to root_url, :notice => "Welcome, #{@user.name}."
 		else
 			flash.now.alert "Your email or password was incorrect."
 			render "new"
