502a03f6d482af54a40dcc1303ffeec8e28f6736
Carly Ho <me@carlymho.com>
2013-05-20 19:43:37 -0500

    Add category management system

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Postit::Application.routes.draw do
   resources :posts, :except => :destroy do
   	resources :comments, :except => :destroy
   end
-
+  resources :categories, :except => :show
   root to: 'posts#index'
 end
